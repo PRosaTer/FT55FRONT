768ad8d7732ef8dd77648c602087dbfa81d7f646
BrenAglr <brenda.sthe.aguilar@gmail.com>
2024-12-10 10:42:41 -0300

    configuracion colores e flaticon

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -11,6 +11,10 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "arena": "#EAE2B7",
+        "primario": "#003049",
+        "secundario": "#FCBF49",
+        "accion": "#D62828"
       },
     },
   },
