c966e3fdeced681a13f1957479898e3a359f5af8
BrenAglr <brenda.sthe.aguilar@gmail.com>
2024-12-13 00:10:18 -0300

    vista detalle sin coneccion back

---
+++ front/src/components/map/index.tsx
@@ -0,0 +1,16 @@
+// react
+import React from 'react';
+
+
+export const Map: React.FC = () => {
+return (
+    <div className="mb-4 lg:mx-40">
+        <h3 className="text-xl font-semibold mb-2">¿Dónde vas a estar?</h3>
+            <div className="h-64 w-full rounded-lg bg-gray-300">
+                Mapa aquí
+            </div>
+    </div>
+)
+}
+
+export default Map
