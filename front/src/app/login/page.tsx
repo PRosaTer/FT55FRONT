986bbd0b527b9585d2d6bbb0347a83cf012b8739
pablo rosa <pabloterosa@gmail.com>
2024-12-13 16:34:11 -0300

    arreglando errors no importantes

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
