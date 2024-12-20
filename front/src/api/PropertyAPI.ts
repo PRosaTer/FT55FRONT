f62646ef963ecf4594890228f29bc760bcf3ddd8
Pablo Rosa <pabloterosa@gmail.com>
2024-12-20 18:52:16 -0300

    tratando de volver a tener todo como lo teniamos antes

--- front/src/api/PropertyAPI.ts
+++ front/src/api/PropertyAPI.ts
@@ -1,24 +1,25 @@
-import { user } from '@/helpers/data';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { IProperty } from '../interfaces/IProperty';
-import { IAccount, IAccountUser } from '@/interfaces/IAccount';
+import { IAccountUser } from '@/interfaces/IAccount';
 
 
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
-export const getPropertyDB = async(): Promise<IProperty[]> => {
+export const getPropertyDB = async (): Promise<IProperty[]> => {
     try {
         const res = await fetch(`${API_URL}/property`, {
-            next: { revalidate: 1200}
+            next: { revalidate: 1200 }
         });
         console.log(res);
-        
+
         if (!res.ok) {
             throw new Error(`Fallo al realizar el fetch a Propiedades: ${res.status} ${res.statusText}`);
         }
         const properties: IProperty[] = await res.json();
         console.log(properties);
-        
+
         return properties;
     } catch (error: any) {
         console.error("Error in getPropertyDB:", error.message || error);
@@ -27,10 +28,10 @@ export const getPropertyDB = async(): Promise<IProperty[]> => {
 
 }
 
-export const getPropertyById = async(id: string): Promise<IProperty> => {
+export const getPropertyById = async (id: string): Promise<IProperty> => {
     try {
         const res = await fetch(`${API_URL}/property/unique/${id}`, {
-            next: { revalidate: 1200}
+            next: { revalidate: 1200 }
         });
 
         if (!res.ok) {
@@ -39,7 +40,7 @@ export const getPropertyById = async(id: string): Promise<IProperty> => {
 
         const property: IProperty[] = await res.json();
         console.log(property[0]);
-        
+
         if (Array.isArray(property) && property.length > 0) {
             return property[0];
         }
@@ -52,19 +53,19 @@ export const getPropertyById = async(id: string): Promise<IProperty> => {
 
 }
 
-export const getPropertyOwner = async(id: string): Promise<IAccountUser> => {
+export const getPropertyOwner = async (id: string): Promise<IAccountUser> => {
     try {
         const res = await fetch(`${API_URL}/account/${id}`, {
-            next: { revalidate: 1200}
+            next: { revalidate: 1200 }
         });
         console.log(res);
-        
+
         if (!res.ok) {
             throw new Error(`Fallo al realizar el fetch a Owner por ID: ${res.status} ${res.statusText}`);
         };
 
-        const owner: IAccountUser= await res.json();
-        
+        const owner: IAccountUser = await res.json();
+
         return owner;
     } catch (error: any) {
         console.error("Error in getPropertyOwner:", error.message || error);
