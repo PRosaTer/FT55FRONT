f74ad9e2e88a2f7d87e1f297e864d4dba1ae235c
BrenAglr <125900892+BrenAglr@users.noreply.github.com>
2025-01-12 19:00:28 -0300

    swal checkout

--- front/src/app/CheckoutPreview/page.tsx
+++ front/src/app/CheckoutPreview/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 // react
 import { useState, useEffect } from "react";
@@ -29,87 +28,85 @@ const CheckoutPreview = () => {
   const router = useRouter(); // Hook para manejar la navegación
   const [loading, setLoading] = useState(false); // Para manejar el estado de carga
 
-  const houseDef = casadef
+  const houseDef = casadef;
   const [reservData, setReservData] = useState<ILocalReservation | null>(null);
   const [propertyData, setPropertyData] = useState<IProperty | null>(null);
-  const [accountData, setAccountData] = useState<string>("")
+  const [accountData, setAccountData] = useState<string>("");
   const [paypalEmail, setPaypalEmail] = useState<string>("");
 
-  useEffect(()=> {
-    
-    const reservLocal = localStorage.getItem('reserv');
-    // Recuperar datos del Local
-    const userLocal = localStorage.getItem('user')
-    if (userLocal) {
-      const user = JSON.parse(userLocal)
-      getUserAccount(user.id)
-      .then((account) => {
-        setAccountData(account.id)
-      })
+  useEffect(() => {
+    // Verificar si existe "compraId" en el localStorage
+    const compraId = localStorage.getItem("compraId");
+    if (compraId) {
+      Swal.fire({
+        icon: "error",
+        title: "Pago fallido",
+        text: "Tu pago anterior no pudo ser procesado. Intenta nuevamente.",
+      }).then(() => {
+        localStorage.removeItem("compraId");
+      });
     }
+
+    const reservLocal = localStorage.getItem("reserv");
+    const userLocal = localStorage.getItem("user");
+
+    if (userLocal) {
+      const user = JSON.parse(userLocal);
+      getUserAccount(user.id).then((account) => {
+        setAccountData(account.id);
+      });
+    }
+
     if (reservLocal) {
       const reserv: ILocalReservation = JSON.parse(reservLocal);
-      setReservData(reserv)
-      
-      // peticion para traerme el email del propietario
+      setReservData(reserv);
+
+      // Petición para traer el email del propietario
       getEmailOwner(reserv.propertyId)
-      .then((email: string) => {
-        setPaypalEmail(email);
-      })
-      .catch((error) => console.error("Error fetching owner email:", error));
+        .then((email: string) => setPaypalEmail(email))
+        .catch((error) => console.error("Error fetching owner email:", error));
 
       // Obtener información de la propiedad desde el backend
-          getPropertyById(reserv.propertyId)
-          .then((property) => {
-              setPropertyData(property);
-              })
-          .catch((error) => console.error('Error fetching property:', error));
-          }
-        }, []);
+      getPropertyById(reserv.propertyId)
+        .then((property) => setPropertyData(property))
+        .catch((error) => console.error("Error fetching property:", error));
+    }
+  }, []);
 
-        if (!reservData || !propertyData) {
-          return <p>Cargando información del viaje...</p>;
-      }
+  if (!reservData || !propertyData) {
+    return <p>Cargando información del viaje...</p>;
+  }
 
-      console.log(reservData);
-      
-      const { dates, prices, travelers, propertyId} = reservData;
-      const {name, type, rating, image_, state, city } = propertyData;
-      console.log(image_);
+  const { dates, prices, travelers, propertyId } = reservData;
+  const { name, type, rating, image_, state, city } = propertyData;
+  const photo = image_ ? image_[0].url : houseDef;
 
-      const photo = image_? image_[0].url : houseDef;
-  
-      const nights = Math.ceil(
-        (new Date(dates.endDate).getTime() - new Date(dates.startDate).getTime()) /
-        (1000 * 60 * 60 * 24)
-          );
+  const nights = Math.ceil(
+    (new Date(dates.endDate).getTime() - new Date(dates.startDate).getTime()) /
+      (1000 * 60 * 60 * 24)
+  );
 
-      const minor = travelers.children || travelers.babies? true : false
+  const minor = travelers.children || travelers.babies ? true : false;
 
-      const reserva = {
-        startDate: dates.startDate,
-        endDate: dates.endDate,
-        guests: travelers.adults + travelers.children,
-        pets: travelers.pets,
-        minor,
-        propertyId,
-        accountId: accountData,
-        paypalEmail,
-      }
+  const reserva = {
+    startDate: dates.startDate,
+    endDate: dates.endDate,
+    guests: travelers.adults + travelers.children,
+    pets: travelers.pets,
+    minor,
+    propertyId,
+    accountId: accountData,
+    paypalEmail,
+  };
 
-      console.log(`esta es la reserva:`,reserva);
-          
-  
   const handlePayment = async () => {
-    setLoading(true); // Activa el estado de carga
+    setLoading(true);
     try {
       const response = await createReservation(reserva);
-  
+
       if (response && response.link) {
-      // guardar el id en local
-      localStorage.setItem("compraId", JSON.stringify(response.id));
-      // Redirigir al usuario al enlace proporcionado por el backend
-     router.push(response.link)
+        localStorage.setItem("compraId", JSON.stringify(response.id));
+        router.push(response.link);
       } else {
         console.error("No se recibió un enlace válido en la respuesta:", response);
         Swal.fire({
@@ -126,26 +123,35 @@ const CheckoutPreview = () => {
         text: "Ocurrió un error inesperado. Intenta nuevamente.",
       });
     } finally {
-      setLoading(false); // Desactiva el estado de carga
+      setLoading(false);
     }
   };
 
   return (
     <div className="flex flex-col md:flex-row justify-between p-6 bg-silk text-black">
-    <CheckoutIzq dates={dates} travelers={travelers}/>
-    <div className="w-full md:w-1/3 bg-pearl p-6 rounded-lg shadow-lg flex flex-col justify-between">
-      <div>
-        <CheckoutDer prices={prices} name={name} type={type} rating={rating} photo={photo} nights={nights} state={state} city={city}/>
+      <CheckoutIzq dates={dates} travelers={travelers} />
+      <div className="w-full md:w-1/3 bg-pearl p-6 rounded-lg shadow-lg flex flex-col justify-between">
+        <div>
+          <CheckoutDer
+            prices={prices}
+            name={name}
+            type={type}
+            rating={rating}
+            photo={photo}
+            nights={nights}
+            state={state}
+            city={city}
+          />
+        </div>
+        <button
+          onClick={handlePayment}
+          className="w-full py-3 bg-champagne text-pearl font-bold rounded-lg hover:bg-velvet hover:text-champagne transition"
+          disabled={loading}
+        >
+          {loading ? "Procesando..." : "Pagar"}
+        </button>
       </div>
-      <button
-        onClick={handlePayment}
-        className="w-full py-3 bg-champagne text-pearl font-bold rounded-lg hover:bg-velvet hover:text-champagne transition"
-        disabled={loading}
-      >
-        {loading ? "Procesando..." : "Pagar"}
-      </button>
     </div>
-  </div>
   );
 };
 
