a88f38b7b7b69fa71b38e332f9838d235ea25337
RocioMigliarese <rociomigliarese3@gmail.com>
2025-01-15 19:36:51 -0300

    fix errors

--- front/src/app/payment/page.tsx
+++
@@ -1,8 +0,0 @@
-// "use client";
-// import SuccessPage from "@/components/successPay/page";
-
-// const ProfilePage: React.FC = () => {
-//   return <SuccessPage />;
-// };
-
-// export default ProfilePage;
