5d58577e1d6763d22232b00ea9bc7bcdddd69afa
cris-hr <cjminisol@gmail.com>
2025-01-16 00:43:33 -0500

    Configuracion ok de navbar para usuario admin, y correcion de estilos en las vistas

--- front/next.config.ts
+++ front/next.config.ts
@@ -18,7 +18,17 @@ const nextConfig: NextConfig = {
       "www.construyehogar.com",
       "www.casasparaconstruir.com",
       "images.homify.com",
-      ""
+      "construcasasprefabricados.com.co",
+      "images.ctfassets.net",
+      "www.centralcasas.cl",
+      "media1.amarilo.com.co",
+      "cache.enlaceinmobiliario.cl",
+      "photos.encuentra24.com",
+      "www.torcaldeantequera.com",
+      "prefabrihouse.com.co",
+      "i.blogs.es",
+      "micasaconstructora.com",
+      "www.insidehome.com.co",
     ],
   },
   /* config options here */
